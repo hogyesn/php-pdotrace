f100712fdc099caa37fc0d565f450721f598ea55
Norbert Hőgyes <hogyesn@gmail.com>
2025-12-25 14:44:29 +0100

    remove test functions

--- extensions/hogyesn/pdotrace/pdotrace.c
+++ extensions/hogyesn/pdotrace/pdotrace.c
@@ -7,7 +7,6 @@
 #include "php.h"
 #include "ext/standard/info.h"
 #include "php_pdotrace.h"
-#include "pdotrace_arginfo.h"
 
 /* For compatibility with older PHP versions */
 #ifndef ZEND_PARSE_PARAMETERS_NONE
@@ -16,32 +15,6 @@
 	ZEND_PARSE_PARAMETERS_END()
 #endif
 
-/* {{{ void test1() */
-PHP_FUNCTION(test1)
-{
-	ZEND_PARSE_PARAMETERS_NONE();
-
-	php_printf("The extension %s is loaded and working!\r\n", "pdotrace");
-}
-/* }}} */
-
-/* {{{ string test2( [ string $var ] ) */
-PHP_FUNCTION(test2)
-{
-	char *var = "World";
-	size_t var_len = sizeof("World") - 1;
-	zend_string *retval;
-
-	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING(var, var_len)
-	ZEND_PARSE_PARAMETERS_END();
-
-	retval = strpprintf(0, "Hello %s", var);
-
-	RETURN_STR(retval);
-}
-/* }}}*/
 
 /* {{{ PHP_RINIT_FUNCTION */
 PHP_RINIT_FUNCTION(pdotrace)
@@ -67,7 +40,7 @@ PHP_MINFO_FUNCTION(pdotrace)
 zend_module_entry pdotrace_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"pdotrace",					/* Extension name */
-	ext_functions,					/* zend_function_entry */
+	NULL,							/* zend_function_entry */
 	NULL,							/* PHP_MINIT - Module initialization */
 	NULL,							/* PHP_MSHUTDOWN - Module shutdown */
 	PHP_RINIT(pdotrace),			/* PHP_RINIT - Request initialization */
